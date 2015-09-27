5526536a80772a09069c4ce78fde2c5471921fd6
Alexis Quintero <alexisquintero@hotmail.com.ar>
2015-09-27 13:07:32 -0300

    Unidad 4 Laboratorio 3
    ADO.Net

--- Ejercicio4/Program.cs
+++ Ejercicio4/Program.cs
@@ -17,19 +17,26 @@ namespace Ejercicio4
             dtEmpresas.Columns.Add("CompanyName", typeof(string));
 
             SqlConnection myconn = new SqlConnection();
-            myconn.ConnectionString = "Data Source=LOCALHOST; Initial Catalog=Northwind;User ID=sa;Pwd=123";
-
+//            myconn.ConnectionString = "Data Source=LOCALHOST; Initial Catalog=Northwind;User ID=sa;Pwd=123";
+            myconn.ConnectionString = "Data Source=localhost;Initial Catalog=Northwind;Integrated Security=True";
+            //Data Source=localhost;Initial Catalog=Northwind;Integrated Security=True
+/*
             SqlCommand mycomando = new SqlCommand();
             mycomando.CommandText = "SELECT CustomerID, CompanyName FROM Customers";
             mycomando.Connection = myconn;
 
-            SqlDataAdapter myadap = new SqlDataAdapter("SELECT CustomerID, CompanyName FROM Customers", myconn);
+//            SqlDataAdapter myadap = new SqlDataAdapter("SELECT CustomerID, CompanyName FROM Customers", myconn);
 
             myconn.Open();
             SqlDataReader mydr = mycomando.ExecuteReader();
             dtEmpresas.Load(mydr);
             myconn.Close();
+*/
+            SqlDataAdapter myadap = new SqlDataAdapter("SELECT CustomerID, CompanyName FROM Customers", myconn);
 
+            myconn.Open();
+            myadap.Fill(dtEmpresas);
+            myconn.Close();
             Console.WriteLine("Listado de Empresas: ");
             foreach (DataRow rowEmpresa in dtEmpresas.Rows)
             {
@@ -37,6 +44,37 @@ namespace Ejercicio4
                 string nombreempresa = rowEmpresa["CompanyName"].ToString();
                 Console.WriteLine(idempresa + " - " + nombreempresa);
             }
+
+            Console.Write("Escriba el CustomerID que desea modificar: ");
+            string custid = Console.ReadLine();
+
+            DataRow[] rwempresas = dtEmpresas.Select("CustomerID = '" + custid + "'");
+            if (rwempresas.Length != 1)
+            {
+                Console.WriteLine("CustomerID no encontrado");
+                Console.ReadLine();
+                return;
+            }
+            DataRow rowMiEmpresa = rwempresas[0];
+            string nombreactual = rowMiEmpresa["CompanyName"].ToString();
+            Console.WriteLine("Nombre actual de la empresa: " + nombreactual);
+            Console.Write("Escriba el nuevo nombre: ");
+            string nuevonombre = Console.ReadLine();
+
+            rowMiEmpresa.BeginEdit();
+            rowMiEmpresa["CompanyName"] = nuevonombre;
+            rowMiEmpresa.EndEdit();
+
+            SqlCommand updcommand = new SqlCommand();
+            updcommand.Connection = myconn;
+            updcommand.CommandText =
+                "UPDATE Customers SET CompanyName = @CompanyName WHERE CustomerID = @CustomerID";
+            updcommand.Parameters.Add("@CompanyName", SqlDbType.NVarChar, 50, "CompanyName");
+            updcommand.Parameters.Add("@CustomerID", SqlDbType.NVarChar, 5, "CUstomerID");
+
+            myadap.UpdateCommand = updcommand;
+            myadap.Update(dtEmpresas);
+
             Console.ReadLine();
         }
     }
